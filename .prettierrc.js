ed00b64872d8025e41fc4fbffc690b04394abdce
wangyang <wangyang@yadingdata.com>
2022-10-25 18:18:06 +0800

    feat: 网格

---
+++ .prettierrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  // 行最大长度
+  printWidth: 200,
+  // tab的宽度
+  tabWidth: 2,
+  // 缩进用Tab
+  useTabs: false,
+  // 行的末尾不需要分号
+  semi: false,
+  // 使用单引号
+  singleQuote: true,
+  // 多行数组是否需要尾部追加逗号 es5 语法需要
+  trailingComma: 'es5',
+  // 定义换行符 lf linux-style
+  endOfLine: 'lf',
+  // HTML的全局空白区域敏感度（解决标签换行问题）
+  htmlWhitespaceSensitivity: 'ignore',
+}
--- src/pages/gridTrade/index.js
+++ src/pages/gridTrade/index.js
@@ -1,11 +1,33 @@
 // 网格交易
+import { Table, Space, Button } from 'antd'
 
+const columns = [
+  { title: '名称', dataIndex: 'name', key: 'name' },
+  { title: '盈亏', dataIndex: 'age', key: 'age' },
+  {
+    title: '操作',
+    key: 'action',
+    render: (_, record) => (
+      <Space size="small">
+        <Button type="link" onClick={() => test(record)}>查看</Button>
+      </Space>
+    ),
+  },
+]
+let data = [
+  {
+    name: '600519',
+    age: -0.9,
+  },
+]
+function test(r) {
+  console.log(r)
+}
 function GridTrade() {
-
   return (
-    <div>
-      网格
-    </div>
+    <>
+      <Table columns={columns} dataSource={data} />
+    </>
   )
 }
 
